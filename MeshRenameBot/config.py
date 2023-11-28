670a226b5ca264b3449b8b68c3752d501a7fee70
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-11-28 18:09:09 +0100

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,26 +4,26 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, "postgres://vyjeeprz:QZJFgX_Q5WXo81HDWoDMnYgMz2nvfRQW@snuffleupagus.db.elephantsql.com/vyjeeprz"]
-        API_HASH = [str, "c0da9c346d2c45dbc7ec49a05da9b2b6"]
-        API_ID = [int, 13675555]
-        BOT_TOKEN = [str, "6680969743:AAHpx2FWxrJDDBZTasyyUk05h7a0zG6aeMc"]
+        DATABASE_URL = [str, "mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority"]
+        API_HASH = [str, "82dcb7e30b20c4e3129c152a53cddb95"]
+        API_ID = [int,"17170223"]
+        BOT_TOKEN = [str, "6835749558:AAEBQQ7jZKlHdnEjpkZV3Lwsmt3nkFqZOwA"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
-        MAX_QUEUE_SIZE = [int, 5]
+        MAX_QUEUE_SIZE = [int, 500]
         SLEEP_SECS = [int, 10]
         IS_MONGO = [bool, False]
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
-        AUTH_USERS = [list,[123456789]]
-        OWNER_ID = [int, 0]
+        AUTH_USERS = [list,[]]
+        OWNER_ID = [int, "1966867320"]
 
         # Public username url or invite link of private chat
         FORCEJOIN = [str,""]
-        FORCEJOIN_ID = [int,-100123465978]
+        FORCEJOIN_ID = [int,]
 
-        TRACE_CHANNEL = [int, 0]
+        TRACE_CHANNEL = [int,"-1001560632964"]
 
 try:
     from .tconfig import Commands
